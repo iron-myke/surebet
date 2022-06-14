b12fa1c8b91a3a14c5713b56855426c9149c033a
Ubuntu <mikhail.bsa@gmaiL.com>
2022-06-14 13:55:06 +0000

    fix

--- track_leagues.sh
+++ track_leagues.sh
@@ -1,4 +1,7 @@
-cd ~/surebet
+#!/usr/bin/sh
+source ~/miniconda3/etc/profile.d/conda.sh
+cd ~/surebet/
 conda activate surebet
 date >> ~/tmp/runs.out
-python track_leagues.py >> ~/tmp/sys.out
+today=$(date +"%Y-%m-%d_%H:%M")
+python track_leagues.py >> ~/tmp/output_${today}.out
